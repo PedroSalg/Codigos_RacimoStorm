5432b2ed60cc20b64f55a1146930453760c5b0a4
Pedro <pedro.salgado179@gmail.com>
2020-04-21 23:02:01 -0500

    Lecutura de datos usando I2C desde la FPGA hacia la RP. Datos de la memoria

--- Python/FPGA_RP.py
+++ Python/FPGA_RP.py
@@ -1,6 +1,6 @@
 import time
-#import RPi.GPIO as GPIO
-from gpiozero import LED
+import RPi.GPIO as GPIO
+from gpiozero import LED, Button
 from smbus2 import SMBus
 
 bus = SMBus(1)
@@ -9,17 +9,39 @@ address = 0x08
 reg0 = 0x00
 
 GPIO.setmode(GPIO.BCM)
-INPUT_PIN = 20
-OUTPUT_PIN = 21
-GPIO.setup(INPUT_PIN, GPIO.IN)
-GPIO.setup(OUTPUT_PIN, GPIO.OUT)
+Entrada = 20
+Salida = 21
+GPIO.setup(Entrada, GPIO.IN)
 
-while True:
-	GPIO.setup(OUTPUT_PIN, False)
-	TO_RP = GPIO.input(INPUT_PIN)
-	print (TO_RP)
-	GPIO.setup(OUTPUT_PIN, True)
-	time.sleep(5)
+
+Led = LED(Salida)
+#Boton = Button(Entrada)
+Led.off()
+
+for i in range(255):
+	Val_Entrada = GPIO.input(Entrada)
+	if (Val_Entrada == True):
+		#print ("3.3 V")
+		#Boton.when_pressed = Led.on
+		dat0 = bus.read_byte_data(address, 0x00)
+		print (dat0)
+		Led.on()
+		time.sleep(0.000001)
+		Led.off()
+		time.sleep(0.000001)
+		#if (Boton.is_pressed):
+	else:
+		print ("PAILA")
+
+#GPIO.setup(INPUT_PIN, GPIO.IN)
+#GPIO.setup(OUTPUT_PIN, GPIO.OUT)
+
+#while True:
+#	GPIO.setup(OUTPUT_PIN, False)
+#	TO_RP = GPIO.input(INPUT_PIN)
+#	print (TO_RP)
+#	GPIO.setup(OUTPUT_PIN, True)
+#	time.sleep(5)
 #	if ( GPIO.input(INPUT_PIN) == True ):
 #		dat0 = bus.read_byte_data(address, 0x00)
 #		print (dat0)
