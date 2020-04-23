42a8045e9c9cb785e644567bc6c6bb8b2135370f
Pedro <pedro.salgado179@gmail.com>
2020-04-22 19:23:55 -0500

    Comunicacion entre RP y FPGA sin protocolo de inicio

--- Python/FPGA_RP.py
+++ Python/FPGA_RP.py
@@ -7,6 +7,7 @@ bus = SMBus(1)
 
 address = 0x08
 reg0 = 0x00
+reg1 = 0x01
 
 GPIO.setmode(GPIO.BCM)
 Entrada = 20
@@ -17,14 +18,19 @@ GPIO.setup(Entrada, GPIO.IN)
 Led = LED(Salida)
 #Boton = Button(Entrada)
 Led.off()
-
-for i in range(255):
+Datos = open("Prueba6.txt","w")
+Data = []
+for i in range(4096):
 	Val_Entrada = GPIO.input(Entrada)
 	if (Val_Entrada == True):
 		#print ("3.3 V")
 		#Boton.when_pressed = Led.on
 		dat0 = bus.read_byte_data(address, 0x00)
-		print (dat0)
+		dat1 = bus.read_byte_data(address, 0x01)
+#		Datos.write(str(dat0) + "\n")
+		Val = (dat1 << 8 ) + dat0
+		Data.append(str(dat0))
+		#print (Val)
 		Led.on()
 		time.sleep(0.000001)
 		Led.off()
@@ -32,6 +38,12 @@ for i in range(255):
 		#if (Boton.is_pressed):
 	else:
 		print ("PAILA")
+#Datos.close()
+
+for i in range(len(Data)):
+	Datos.write(Data[i] + '\n')
+	#print (Data[i])
+Datos.close()
 
 #GPIO.setup(INPUT_PIN, GPIO.IN)
 #GPIO.setup(OUTPUT_PIN, GPIO.OUT)
