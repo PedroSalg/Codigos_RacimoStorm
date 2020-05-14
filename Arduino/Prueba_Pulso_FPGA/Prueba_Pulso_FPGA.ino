7fd84c7ab7c2fcbcac7c3fe79b04cfe145f0cbf7
PedroSalg <pedro.salgado179@gmail.com>
2020-05-14 13:17:47 -0500

    Código de arduino para general pulso cuadrado de ancho variable para pruebas de deteccion

---
+++ Arduino/Prueba_Pulso_FPGA/Prueba_Pulso_FPGA.ino
@@ -0,0 +1,26 @@
+const int Boton = 7;
+const int Led = 13;
+int Lectura_Boton = 1;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(Boton, INPUT);
+  pinMode(Led, OUTPUT);
+ 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Lectura_Boton = digitalRead(Boton);
+  if (Lectura_Boton == HIGH){
+    digitalWrite(Led, HIGH);
+    delay(0.4);
+    digitalWrite(Led, LOW);
+    delay(200);
+    }
+  else {
+    digitalWrite(Led, LOW);
+    }
+
+}
