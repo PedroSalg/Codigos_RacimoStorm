1800a35a2418253e768934459f179e7f367bb094
leonardo-florez <57200703+leonardo-florez@users.noreply.github.com>
2020-06-14 16:00:12 -0500

    Add files via upload

--- Python/dataGPS.py
+++ Python/dataGPS.py
@@ -1,4 +1,4 @@
-#### ADQUISICIÓN DE LOS DATOS DEL GPS ####
+#### ADQUISICION DE LOS DATOS DEL GPS ####
 
 # IMPORTAMOS LIBRERIAS
 ##########################################
@@ -10,14 +10,19 @@ import os.path
 import venusGPS
 
 
-# INICIALIZACIÓN
+# INICIALIZACIODA   N
 portgps = "/dev/serial0"
 baudrategps = 9600
 timeoutgps = 0.5
 dgps = venusGPS.dataVec(portgps,baudrategps,timeoutgps)
 
+# TIMEOUT
+timeS = time.time()
+
 while True:
-    print("Esperando GPS.....")
+    print("Esperando GPS: " + venusGPS.available(dgps))
+    #TIEMPO TRANSCURRIDO
+    timeF = time.time()
     if venusGPS.available(dgps) == "A":
         #UNIX TIME
         ut= float(venusGPS.unixTime(data))
@@ -37,6 +42,7 @@ while True:
         Datos = str(ut) + ' ' +str(lat) + ' ' + str(dirlat) + ' ' + str(lon) + ' ' + str(dirlon) + ' ' + str(alt) + ' \n'
         #LINEA DE BASH PARA ACTUALIZAR FECHA
         Line = "sudo date --set @" + str(ut)
+        
         if os.path.isfile(dataGPS):
             bool = True
             archivo = open(dataGPS,'a')
@@ -44,6 +50,7 @@ while True:
             archivo.write(Datos)
             archivo.close()
             os.system(Line)
+            break
         else:
             bool = False
             archivo = open(dataGPS,'w')
@@ -51,3 +58,8 @@ while True:
             archivo.write(Datos)
             archivo.close()
             os.system(Line)
+            break
+        
+    elif timeF-timeS > 60.0:
+        print("Señal de GPS invalida, no se actualizo dateGPS.txt")
+        break
